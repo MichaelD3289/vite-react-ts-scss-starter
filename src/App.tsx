f8ffb973e0c9e9927adffb541c8671f7a6c51dce
Michael Drummond <michael.drummond328@gmail.com>
2022-12-23 15:57:04 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './views/Home';
+import NotFound from './views/NotFound';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/views/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <h1 className="clr-primary-400 text-center padding-block-24">
+      Hello World
+    </h1>
+  );
+}
+
+export default Home;
